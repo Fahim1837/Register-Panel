3d28dd11d26ec7dd5176340c78d79fe5d31d4970
Fahim1837 <fahimrb37@gmail.com>
2024-02-02 03:09:14 +0600

    Creating Initial Styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import FormPage from "./FormPage";
 
 function App() {
   return (
-    <div >App with Tailwind</div>
-  )
+    <div className="h-screen bg-slate-400 flex items-center justify-center">
+      <div className="bg-slate-100 p-5 flex flex-col items-center justify-center space-y-3 rounded-xl shadow-lg ">
+        <h1 className="font-bold text-purple-800 text-2xl ">Register</h1>
+        <form className="space-y-2">
+          <FormPage />
+          <FormPage />
+          <FormPage />
+          <FormPage />
+          <FormPage />
+          <div className="flex justify-center">
+            <button>Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/FormPage.jsx
@@ -0,0 +1,10 @@
+function FormPage() {
+  return (
+    <div className="flex flex-col">
+      <label htmlFor="">Username</label>
+      <input type="text" placeholder="Username" />
+    </div>
+  );
+}
+
+export default FormPage;
