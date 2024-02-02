c7bcecd215be3ac2d1a1523488ee35510680a5a8
Fahim1837 <fahimrb37@gmail.com>
2024-02-02 09:55:50 +0600

    classname shortened

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import FormPage from "./FormPage";
 
 function App() {
   return (
-    <div className="h-screen bg-slate-400 flex items-center justify-center">
-      <div className="bg-slate-100 p-5 flex flex-col items-center justify-center space-y-3 rounded-xl shadow-lg ">
-        <h1 className="font-bold text-purple-800 text-2xl ">Register</h1>
-        <form className="space-y-2">
+    <div className="cont">
+      <div className="card">
+        <h1 className="register">Register</h1>
+        <form className="space-y-3 ">
           <FormPage />
           <FormPage />
           <FormPage />
           <FormPage />
           <FormPage />
-          <div className="flex justify-center">
-            <button>Submit</button>
+          <div className="flex justify-center pt-3">
+            <button className="btn">Submit</button>
           </div>
         </form>
       </div>
